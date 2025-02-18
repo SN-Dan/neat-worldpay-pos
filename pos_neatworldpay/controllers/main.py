4fc80bbbb6b6ad43ed18c73342dc4819cc117b15
jefri931 <32366316+jefri931@users.noreply.github.com>
2025-02-18 08:15:30 +0200

    fixed pos_payment_method model and cleaned up the controller

--- pos_neatworldpay/controllers/main.py
+++ pos_neatworldpay/controllers/main.py
@@ -2,120 +2,11 @@
 import logging
 import json
 from odoo import http
-from odoo.http import request
-from datetime import datetime, timedelta
-from werkzeug.security import check_password_hash
-from .config import timeout_delta
-from .payment_processor import is_order_the_same, create_payment_request, cancel_payment_request, check_request, request_processed, get_payment_history
-from cryptography.fernet import Fernet
+from .payment_processor import is_order_the_same, create_payment_request, cancel_payment_request, check_request, request_processed
 
 _logger = logging.getLogger(__name__)
 
-
 class PosNeatWorldpayController(http.Controller):
-
-    def decrypt(self, token, fernet_key):
-        fernet = Fernet(fernet_key)
-        return fernet.decrypt(token.encode()).decode()
-
-    def encrypt(self, message, fernet_key):
-        fernet = Fernet(fernet_key)
-        return fernet.encrypt(message.encode()).decode()
-    def get_secret_key(self):
-        sc_rows = request.env['neat.worldplay.security'].sudo().search([]).read(['neat_worldpay_secret_key'])
-        if len(sc_rows) == 0:
-            pr = request.env['neat.worldplay.security'].sudo().create({'neat_worldplay_secret_key': 'str'})
-            secret_key = pr.neat_worldplay_secret_key
-        else:
-            secret_key = sc_rows[0]['neat_worldplay_secret_key']
-        return secret_key
-
-    def generate_tokens(self, device_code, amount, transaction_id, start_date, is_refund, is_from_initial_request):
-        pos_payment_methods = http.request.env['pos.payment.method'].sudo().search(
-            [('neat_worldplay_terminal_device_code', '=', device_code)]).read(['neat_worldplay_terminal_pass_uuid'])
-        if len(pos_payment_methods) == 0:
-            return {'status': 404}
-        payload = {
-            'device_code': device_code,
-            'pass_uuid': pos_payment_methods[0]['neat_worldplay_terminal_pass_uuid'],
-            'amount': amount,
-            'transaction_id': transaction_id,
-            'start_date': start_date.timestamp(),
-            'is_refund': is_refund,
-            'is_from_initial_request': is_from_initial_request
-        }
-
-        secret_key = self.get_secret_key()
-        token = self.encrypt(str(json.dumps(payload)), secret_key)
-        return { 'payment_token': token, 'refresh_token': self.generate_refresh_token(device_code) }
-    def auth_payment_token(self, payment_token, tid, amt, is_old_payment_token):
-        secret_key = self.get_secret_key()
-        decoded_token = json.loads(self.decrypt(payment_token, secret_key))
-        device_code = decoded_token.get('device_code')
-        pass_uuid = decoded_token.get('pass_uuid')
-        start_date = datetime.utcfromtimestamp(decoded_token.get('start_date'))
-        amount = decoded_token.get('amount')
-        is_refund = decoded_token.get('is_refund')
-        transaction_id = decoded_token.get('transaction_id')
-        if is_old_payment_token:
-            if not decoded_token['is_from_initial_request']:
-                return { 'authenticated': False }
-        else:
-            current_datetime = datetime.utcnow()
-            max_delta = timeout_delta
-            delta = current_datetime - start_date
-            if delta > max_delta:
-                return { 'authenticated': False }
-
-        if tid != transaction_id:
-            return {'authenticated': False}
-        if amt != amount:
-            return {'authenticated': False}
-        search_domain = [('neat_worldplay_terminal_device_code', '=', device_code)]
-        if not is_old_payment_token:
-            search_domain.append(('neat_worldplay_terminal_pass_uuid', '=', pass_uuid))
-        pos_payment_methods = http.request.env['pos.payment.method'].sudo().search(search_domain).read(['neat_worldplay_terminal_device_code'])
-        if len(pos_payment_methods) == 0:
-            return { 'authenticated': False }
-        current_payment_requests = http.request.env['neat.worldplay.payment.request'].sudo().search(
-            [('terminal_id', '=', device_code), ('transaction_id', '=', transaction_id), ('amount', '=', amount), ('status', 'not like', 'processed_%')]).read(['status'])
-        if len(current_payment_requests) == 0:
-            return { 'authenticated': False }
-
-        return { 'authenticated': True, 'device_code': device_code, 'transaction_id': transaction_id, 'amount': amount, 'is_refund': is_refund }
-    def generate_refresh_token(self, device_code):
-        pos_payment_methods = http.request.env['pos.payment.method'].sudo().search(
-            [('neat_worldplay_terminal_device_code', '=', device_code)]).read(['neat_worldplay_terminal_pass_uuid'])
-        if len(pos_payment_methods) == 0:
-            return {'status': 404}
-        payload = {
-            'device_code': device_code,
-            'timestamp': datetime.utcnow().timestamp(),
-            'pass_uuid': pos_payment_methods[0]['neat_worldplay_terminal_pass_uuid']
-        }
-
-        secret_key = self.get_secret_key()
-        token = self.encrypt(str(json.dumps(payload)), secret_key)
-        return token
-    def auth_refresh_token(self, refresh_token):
-        secret_key = self.get_secret_key()
-        decoded_token = json.loads(self.decrypt(refresh_token, secret_key))
-        device_code = decoded_token['device_code']
-        timestamp = decoded_token['timestamp']
-        pass_uuid = decoded_token['pass_uuid']
-        pos_payment_methods = http.request.env['pos.payment.method'].sudo().search(
-            [('neat_worldplay_terminal_device_code', '=', device_code), ('neat_worldplay_terminal_pass_uuid', '=', pass_uuid)]).read(['neat_worldplay_terminal_device_code'])
-        if len(pos_payment_methods) == 0:
-            return { 'authenticated': False }
-        token_datetime = datetime.utcfromtimestamp(timestamp)
-        current_datetime = datetime.utcnow()
-        max_delta = timedelta(days=180)
-        delta = current_datetime - token_datetime
-        if delta <= max_delta:
-            return { 'authenticated': True, 'device_code': device_code }
-        else:
-            return { 'authenticated': False, 'device_code': device_code }
-
     @http.route('/neat_worldplay/is_order_the_same', type='json', auth='user', methods=['POST'])
     def is_order_the_same(self, refunded_order_line_ids):
         return is_order_the_same(refunded_order_line_ids)
@@ -138,57 +29,8 @@ class PosNeatWorldpayController(http.Controller):
 
     @http.route('/neat_worldplay/validate_connection', type='http', auth='public', methods=['POST'], csrf=False, cors='*')
     def validate_connection(self):
-        r = json.loads(http.request.httprequest.data)
-        device_code = r['device_code']
-        master_password = r['master_password']
-        refresh_token = r.get('refresh_token', None)
-        if refresh_token:
-            auth_result = self.auth_refresh_token(refresh_token)
-            if auth_result['device_code'] != device_code:
-                return json.dumps({'status': 403})
-        pos_payment_methods = http.request.env['pos.payment.method'].sudo().search([('neat_worldplay_terminal_device_code', '=', device_code)]).read(['name', 'neat_worldplay_terminal_master_pwd'])
-        if len(pos_payment_methods) == 0:
-            return json.dumps({ 'status': 404 })
+        return json.dumps({ 'status': 200 })
 
-        if not check_password_hash(pos_payment_methods[0]['neat_worldplay_terminal_master_pwd'], master_password):
-            return json.dumps({ 'status': 401 })
-        token = self.generate_refresh_token(device_code)
-        return json.dumps({ 'status': 200, 'data': { 'refresh_token': token } })
-
-    @http.route('/neat_worldplay/payment_history', type='http', auth='public', methods=['POST'], csrf=False, cors='*')
-    def payment_history(self):
-        request_data = json.loads(http.request.httprequest.data)
-        refresh_token = request_data.get('refresh_token')
-        start_date_text = request_data.get('start_date')
-        end_date_text = request_data.get('end_date')
-        page = request_data.get('page') or 1
-        filter_value = request_data.get('filter', '')
-
-        date_format = "%Y-%m-%d"
-        start_date = datetime.strptime(start_date_text, date_format)
-        end_date = datetime.strptime(end_date_text, date_format)
-        auth_result = self.auth_refresh_token(refresh_token)
-        if not auth_result['authenticated']:
-            return json.dumps({'status': 401})
-
-        terminal_id = auth_result['device_code']
-        response_data = get_payment_history(start_date, end_date, page, filter_value, terminal_id)
-
-        return json.dumps(response_data)
-
-    @http.route('/neat_worldplay/info',type='http', auth='public', methods=['POST'], csrf=False, cors='*')
-    def info(self):
-        r = json.loads(http.request.httprequest.data)
-        refresh_token = r['refresh_token']
-        res = self.auth_refresh_token(refresh_token)
-        if not res['authenticated']:
-            return json.dumps({ 'status': 401 })
-        pos_payment_method = http.request.env['pos.payment.method'].sudo().search(
-            [('neat_worldplay_terminal_device_code', '=', res['device_code'])]).read(['name'])
-        if len(pos_payment_method) == 0:
-            return json.dumps({ 'status': 404 })
-
-        return json.dumps({ 'status': 200, 'data': { 'name': pos_payment_method[0]['name'] } })
 
 
 
--- pos_neatworldpay/models/pos_payment_method.py
+++ pos_neatworldpay/models/pos_payment_method.py
@@ -21,38 +21,43 @@ class PosPaymentMethod(models.Model):
 
     def _get_payment_terminal_selection(self):
         return super(PosPaymentMethod, self)._get_payment_terminal_selection() + [('neatworldplay', 'Neat Worldplay Terminal')]
-    ## POS license key, pos id, pos reference, pos activation code(one time password), publisher id(one per pos)
-    neat_worldplay_terminal_device_code = fields.Char('Terminal ID', help='This is a uniquely generated identifier upon creation for each terminal used to login. The same Terminal ID should never be used for more than 1 terminal')
-    neat_worldplay_terminal_master_pwd = fields.Char('Terminal Master Password', help='', groups="base.group_erp_manager")
-    neat_worldplay_terminal_master_pwd_mock = fields.Char('Terminal Master Password', help='Password used to login in the terminal', default='', groups="base.group_erp_manager")
-    neat_worldplay_terminal_pass_uuid = fields.Char('Terminal Pass UUID')
+
+    neat_worldpay_pos_license_key = fields.Char('POS License Key', help='Generated from other values and refreshed after some time.')
+    neat_worldpay_pos_id = fields.Char('POS ID', help='This is for Worldpay to reference and track POS registrations for internal consolidation and reconciliation.')
+    neat_worldpay_pos_reference = fields.Char('POS Reference',
+                                       help='The POS reference entered during the POS activation in the IPS Control Center')
+    neat_worldpay_pos_activation_code = fields.Char('POS Activation Code',
+                                       help='Activation code generated during the POS activation. Acts as a one time password')
+    neat_worldplay_terminal_device_code = fields.Char('Publisher ID', help='This is a uniquely generated identifier upon creation for each terminal used to login. The same Publisher ID should never be used for more than 1 terminal')
 
     @api.model
     def create(self, values):
         if values['use_payment_terminal'] == 'neatworldplay':
-            if values['neat_worldplay_terminal_master_pwd_mock'] is False or values['neat_worldplay_terminal_master_pwd_mock'] == '' or len(
-                values['neat_worldplay_terminal_master_pwd_mock']) < 6:
-                raise ValidationError("Master Password must have at least 6 characters.")
-            values['neat_worldplay_terminal_pass_uuid'] = self.generate_password_uuid()
-            hashed_password = generate_password_hash(values['neat_worldplay_terminal_master_pwd_mock'])
-            values['neat_worldplay_terminal_master_pwd'] = hashed_password
-            del values['neat_worldplay_terminal_master_pwd_mock']
+            values['sn_zettle_terminal_device_code'] = self.generate_unique_datetime_id()
         return super(PosPaymentMethod, self).create(values)
 
     def write(self, values):
-        if 'neat_worldplay_terminal_master_pwd_mock' in values:
-            if values['neat_worldplay_terminal_master_pwd_mock'] is False or values[
-                'neat_worldplay_terminal_master_pwd_mock'] == '' or len(
-                    values['neat_worldplay_terminal_master_pwd_mock']) < 6:
-                raise ValidationError("Master Password must have at least 6 characters.")
-            values['neat_worldplay_terminal_pass_uuid'] = self.generate_password_uuid()
-            hashed_password = generate_password_hash(values['neat_worldplay_terminal_master_pwd_mock'])
-            values['neat_worldplay_terminal_master_pwd'] = hashed_password
-            del values['neat_worldplay_terminal_master_pwd_mock']
+        if 'sn_zettle_terminal_device_code' in values:
+            del values['sn_zettle_terminal_device_code']
 
         return super(PosPaymentMethod, self).write(values)
+    def generate_unique_datetime_id(self):
+        while True:
+            # Generate a datetime string with milliseconds and the full year
+            current_datetime = datetime.utcnow().strftime('%Y%m%d%H%M%S%f')[:-3]
 
-    def generate_password_uuid(self):
-        return datetime.utcnow().strftime('%Y%m%d%H%M%S') + str(random.randint(1000, 9999))
+            # Replace numbers with corresponding letters for uneven indexes
+            transformed_id = ''.join(
+                [chr(ord('a') + int(char)) if index % 2 != 0 else char for index, char in enumerate(current_datetime)])
+
+            # Insert dashes every 3 characters
+            formatted_id = '-'.join([transformed_id[i:i+3] for i in range(0, len(transformed_id), 3)])
+
+            # Check if the generated ID already exists in the database
+            existing_record = self.search([('sn_zettle_terminal_device_code', '=', formatted_id)])
+
+            # If no matching record found, return the unique ID
+            if not existing_record:
+                return formatted_id
 
 
