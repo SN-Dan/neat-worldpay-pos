1425c07f17abb87640b25f9c6cc3c3436a575255
SN-Dan <152906479+SN-Dan@users.noreply.github.com>
2025-07-10 16:26:02 +0300

    init 18

--- pos 16/pos_neatworldpay/static/src/js/models.js
+++
@@ -1,9 +0,0 @@
-odoo.define('pos_neatworldpay.models', function(require) {
-    var models = require('point_of_sale.models');
-    var PaymentTerminal = require('pos_neatworldpay.payment');
-    models.register_payment_method('neatworldpay', PaymentTerminal);
-    if(models.load_fields) {
-        models.load_fields('pos.payment.method', ['neat_worldpay_terminal_device_code', 'neat_worldpay_is_mobile', 'neat_worldpay_is_desktop_mode', 'neat_worldpay_is_local_ws_server', 'neat_worldpay_ws_url', 'neat_worldpay_is_terminal_printer_communication_allowed', 'neat_worldpay_device_type']);
-    }
-});
-
--- pos 17/pos_neatworldpay/controllers/config.py
+++
@@ -1,5 +0,0 @@
-# Original Author: Daniel Stoynev
-# Copyright (c) 2025 SNS Software Ltd. All rights reserved.
-from datetime import timedelta
-
-timeout_delta = timedelta(minutes=3, seconds=0)
