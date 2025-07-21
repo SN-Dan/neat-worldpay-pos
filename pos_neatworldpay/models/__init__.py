6381b7a601bb7d3adc9fc9a71f6cada4c0bc5a9b
SN-Dan <152906479+SN-Dan@users.noreply.github.com>
2025-07-21 08:56:44 +0300

    Corrected licensing

--- pos_neatworldpay/models/__init__.py
+++ pos_neatworldpay/models/__init__.py
@@ -1,6 +1,8 @@
 # coding: utf-8
 # Original Author: Daniel Stoynev
 # Copyright (c) 2025 SNS Software Ltd. All rights reserved.
+# This module extends Odoo's payment framework.
+# Odoo is a trademark of Odoo S.A.
 
 from . import pos_payment_method
 from . import pos_session
