8bfd59e41db5680c443b99da27269cc916ed912b
SN-Dan <152906479+SN-Dan@users.noreply.github.com>
2025-07-24 15:19:17 +0300

    Added main.gif

--- pos_neatworldpay/__manifest__.py
+++ pos_neatworldpay/__manifest__.py
@@ -21,7 +21,7 @@
     },
     'depends': ['point_of_sale'],
     'qweb': [],
-    'images': ['static/description/landscape Neat POS.gif'],
+    'images': ['static/description/main.gif'],
     'assets': {
         'point_of_sale._assets_pos': [
             'pos_neatworldpay/static/**/*',
